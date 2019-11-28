b30c27a41e85e75d96435843924fd0b1855283d7
Ale <aleevelarde9@gmail.com>
2019-11-28 16:41:53 -0600

    metodo crearGasto añadido

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
 
 class FormularioGasto extends Component {
+
+    //refs para los campos del formulario
+    nombreGasto= React.createRef();
+    cantidadGasto= React.createRef();
+
+    crearGasto = (e) => {
+        //prevenir el default
+        e.preventDefault();
+        //Crear el objeto con los datos
+        const gasto = {
+            nombreGasto: this.nombreGasto.current.value,
+            cantidadGasto: this.cantidadGasto.current.value
+        } 
+        console.log(gasto);
+        //agregarlo y enviarlo con props
+
+        //resetear el formulario (opcional)
+        e.currentTarget.reset();
+    }
     render() {
         return(
-        <form>
+        <form onSubmit={this.crearGasto}>
             <h2>Agrega tus gastos aqui</h2>
             <div className="campo">
                 <label>Nombre Gasto</label>
-                <input className="u-full-width" type="text" placeholder="Ej. Transporte" />
+                <input ref={this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte" />
             </div>
         
             <div className="campo">
                 <label>Cantidad</label>
-                <input className="u-full-width" type="text" placeholder="Ej. 300" />
+                <input ref={this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300" />
             </div>
         
             <input className="button-primary u-full-width" type="submit" value="Agregar" />
