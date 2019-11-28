eb034de9fa701e9546e6c5b887d8e0be527ed5b2
Ale <aleevelarde9@gmail.com>
2019-11-28 15:30:55 -0600

    index & app.js modificado

---
+++ src/componentes/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <p>hola mundo</p>
+    );
+  }
+  
+}
+
+export default App;
