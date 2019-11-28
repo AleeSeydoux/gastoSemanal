81b41211c608a972d93ccf525f472159d8baac8e
Ale <aleevelarde9@gmail.com>
2019-11-28 15:43:12 -0600

    Header.js añadido

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
 import '../css/App.css';
+import Header from './Header';
 
 class App extends Component {
   render(){
     return(
-      <p>hola mundo</p>
+      <div className="App container">
+        <Header
+          titulo='Gasto Semanal'
+        />
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column">
+1
+            </div>
+            <div className="one-half column">
+2
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     );
   }
   
