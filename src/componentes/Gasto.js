f45dafd6ceda7e0617fa9a7b4f8c34298ee4684c
AleeSeydoux <aleevelarde9@gmail.com>
2019-12-05 13:20:36 -0600

    Gasto.js añadido

---
+++ src/componentes/Gasto.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Gasto extends Component {
+    render() {
+        return(
+            <p>desde gasto </p>
+        )
+    }
+}
+export default Gasto;
