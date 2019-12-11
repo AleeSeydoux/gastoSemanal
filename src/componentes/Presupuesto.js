aa80ec8672df8d7073b82a2e6283e40eccac66bb
AleeSeydoux <aleevelarde9@gmail.com>
2019-12-11 11:48:04 -0600

    actualizado

---
+++ src/componentes/Presupuesto.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Presupuesto extends Component {
+    render(){
+        return(
+            <p></p>
+        )
+    }
+}
+export default Presupuesto; 
