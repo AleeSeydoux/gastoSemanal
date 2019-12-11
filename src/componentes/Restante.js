97193e6f2d095dcb7d31dafec3f90300cd68df31
Ale <aleevelarde9@gmail.com>
2019-12-11 15:52:35 -0600

    proptypes en restante

--- src/componentes/Restante.js
+++ src/componentes/Restante.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {revisarPresupuesto} from '../helper';
+import PropTypes from 'prop-types';
 
 class Restante extends Component {
     render(){
@@ -14,5 +15,10 @@ class Restante extends Component {
             </div>
         )
     }
+}
+
+Restante.propTypes = {
+    presupuesto: PropTypes.string.isRequired,
+    restante: PropTypes.string.isRequired
 }
 export default Restante; 
