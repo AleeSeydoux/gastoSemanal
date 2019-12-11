4e4d5be1852f5cd791ea385141fc9bbb9d9400e5
AleeSeydoux <aleevelarde9@gmail.com>
2019-12-11 11:02:34 -0600

    ControlPresupuesto.js añadido

---
+++ src/componentes/ControlPresupuesto.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ControlPresupuesto extends Component {
+    render(){
+        return(
+            <p>desde control</p>
+        )
+    }
+}
+export default ControlPresupuesto;
